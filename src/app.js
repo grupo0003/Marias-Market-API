a098776c8b66436e225dddb72911adecb96b3f3f
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-01-19 22:21:19 -0300

    feat: app wait mongo connection

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
-const { Router } = require('express')
 const express = require('express')
-const router = require('./routes')
+
+const Database = require('./infra/database/mongo/index')
 
 class App {
-  constructor () {
-    this.server = express()
+  static async init () {
+    if (!this._app) {
+      this._app = new App()
+      this._app.express = express()
+
+      await Database.init()
+
+      this._app.middlewares()
+      this._app.routes()
+    }
+
+    return this._app.express
   }
 
   middlewares () {
-    this.server.use(express.json())
+    this.express.use(express.urlencoded({ extended: true }))
+    this.express.use(express.json())
   }
 
   routes () {
-    Router(this.server)
   }
 }
 
-module.exports = new App().server
+module.exports = App
