51c15b76c051fa847a9f6ec499628c2a5a00aa46
Diogo Alexandre <68453937+diogo-alexandre@users.noreply.github.com>
2022-01-20 14:57:16 -0300

    Revert "Story/ec176fb/update post1"

--- src/app/repository/EmployeeRepository.js
+++
@@ -1,9 +0,0 @@
-const schema = require('../schema/employeeSchema')
-
-class EmployeeRepository {
-  async create (employee) {
-    return schema.create(employee)
-  }
-}
-
-module.exports = new EmployeeRepository()
--- src/app/repository/ProductRepository.js
+++
@@ -1,9 +0,0 @@
-const schema = require('../schema/productSchema')
-
-class ProductRepository {
-  async create (product) {
-    return schema.create(product)
-  }
-}
-
-module.exports = new ProductRepository()
