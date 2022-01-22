f5666752a6e77ca789c8e86a300ed7051bf3c750
Diogo Alexandre <68453937+diogo-alexandre@users.noreply.github.com>
2022-01-22 11:08:31 -0300

    ref: local variables should be lower case

--- src/app/helper/isCpf.js
+++ src/app/helper/isCpf.js
@@ -1,21 +1,21 @@
 function isValid (cpfNumber) {
-  let Sum = 0
-  let Rest
+  let sum = 0
+  let rest
 
   if (cpfNumber === '00000000000' || cpfNumber.length !== 11) return false
 
-  for (let i = 1; i <= 9; i++) Sum = Sum + parseInt(cpfNumber.substring(i - 1, i)) * (11 - i)
-  Rest = (Sum * 10) % 11
+  for (let i = 1; i <= 9; i++) sum = sum + parseInt(cpfNumber.substring(i - 1, i)) * (11 - i)
+  rest = (sum * 10) % 11
 
-  if ((Rest === 10) || (Rest === 11)) Rest = 0
-  if (Rest !== parseInt(cpfNumber.substring(9, 10))) return false
+  if ((rest === 10) || (rest === 11)) rest = 0
+  if (sum !== parseInt(cpfNumber.substring(9, 10))) return false
 
-  Sum = 0
-  for (let i = 1; i <= 10; i++) Sum = Sum + parseInt(cpfNumber.substring(i - 1, i)) * (12 - i)
-  Rest = (Sum * 10) % 11
+  sum = 0
+  for (let i = 1; i <= 10; i++) sum = sum + parseInt(cpfNumber.substring(i - 1, i)) * (12 - i)
+  rest = (sum * 10) % 11
 
-  if ((Rest === 10) || (Rest === 11)) Rest = 0
-  if (Rest !== parseInt(cpfNumber.substring(10, 11))) return false
+  if ((rest === 10) || (rest === 11)) rest = 0
+  if (rest !== parseInt(cpfNumber.substring(10, 11))) return false
   return true
 }
 
