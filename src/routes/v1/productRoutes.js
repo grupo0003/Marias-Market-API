4b6cfce41e5eb78081ffe2ede270f5e27d43c718
br3nds0n <brendson.campos_bols@compasso.com.br>
2022-01-22 02:42:11 -0300

    feat: validation UUID post product

--- src/routes/v1/productRoutes.js
+++ src/routes/v1/productRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const ProductController = require('../../app/controller/productController')
-
+const createValidate = require('../../app/validation/product/create')
 router
   .get('/product', ProductController.list)
+  .post('/product', createValidate, ProductController.create)
 
 module.exports = router
