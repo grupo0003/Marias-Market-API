8d34db11cb14017ce857744a25dbbd0547bfa06b
Diogo Alexandre <68453937+diogo-alexandre@users.noreply.github.com>
2022-01-22 11:01:44 -0300

    Update productRoutes.js

--- src/routes/v1/productRoutes.js
+++ src/routes/v1/productRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const ProductController = require('../../app/controller/productController')
 const createValidate = require('../../app/validation/product/create')
+
 router
   .get('/product', ProductController.list)
   .post('/product', createValidate, ProductController.create)
