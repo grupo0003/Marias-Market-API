c3f8a01ca5343bea77b813b248963e846135d014
br3nds0n <brendson.campos_bols@compasso.com.br>
2022-01-23 11:42:22 -0300

    feat: validation query params delete employee

--- src/routes/v1/employeeRouter.js
+++ src/routes/v1/employeeRouter.js
@@ -8,6 +8,6 @@ router
   .get('/employee', EmployeeController.list)
   .post('/employee', createValidate, EmployeeController.create)
   .put('/employee/:id', validateParams, updateValidate, EmployeeController.update)
-  .delete('/employee/:id', EmployeeController.delete)
+  .delete('/employee/:id', validateParams, EmployeeController.delete)
 
 module.exports = router
