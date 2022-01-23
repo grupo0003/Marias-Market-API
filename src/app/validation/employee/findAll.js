8e72adc9686289f68639eef36943cff61ddd1056
Felipe Bezerra <felipebnews@gmail.com>
2022-01-23 13:35:47 -0300

    feat: findAll employee validation

---
+++ src/app/validation/employee/findAll.js
@@ -0,0 +1,40 @@
+const isCpf = require('../../helper/isCpf')
+const JoiDate = require('@joi/date')
+const Joi = require('joi').extend(JoiDate)
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      name: Joi.string(),
+
+      cpf: Joi.string()
+        .min(11)
+        .max(11)
+        .custom((value, help) => {
+          if (isCpf(value)) {
+            return help.message('Cpf inválido')
+          } else {
+            return true
+          }
+        }),
+
+      office: Joi.string()
+        .valid('gerente', 'vendedor', 'caixa'),
+
+      birthday: Joi.date()
+        .max('now')
+        .format('DD/MM/YYYY')
+        .max('now'),
+
+      situation: Joi.string()
+        .default('activate')
+    })
+
+    const { error } = await schema.validate(req.params, { abortEarl: true })
+
+    if (error) throw error
+    return next()
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+}
--- src/routes/v1/employeeRouter.js
+++ src/routes/v1/employeeRouter.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const EmployeeController = require('../../app/controller/employeeController')
 const createValidate = require('../../app/validation/employee/create')
 const updateValidate = require('../../app/validation/employee/update')
+const findAllValidate = require('../../app/validation/employee/findAll')
 const validateParams = require('../../app/validation/employee/queryparams')
 
 router
-  .get('/employee', EmployeeController.list)
+  .get('/employee', findAllValidate, EmployeeController.list)
   .post('/employee', createValidate, EmployeeController.create)
   .put('/employee/:id', validateParams, updateValidate, EmployeeController.update)
   .delete('/employee/:id', validateParams, EmployeeController.delete)
