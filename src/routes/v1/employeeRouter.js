8eb5aeb2006ce58d82c7f6ea32d483c3c95bf32a
br3nds0n <brendson.campos_bols@compasso.com.br>
2022-01-23 10:55:52 -0300

    feat: validation query params update

--- src/routes/v1/employeeRouter.js
+++ src/routes/v1/employeeRouter.js
@@ -2,11 +2,12 @@ const router = require('express').Router()
 const EmployeeController = require('../../app/controller/employeeController')
 const createValidate = require('../../app/validation/employee/create')
 const updateValidate = require('../../app/validation/employee/update')
+const validateParams = require('../../app/validation/employee/queryparams')
 
 router
   .get('/employee', EmployeeController.list)
   .post('/employee', createValidate, EmployeeController.create)
-  .put('/employee/:id', updateValidate, EmployeeController.update)
+  .put('/employee/:id', validateParams, updateValidate, EmployeeController.update)
   .delete('/employee/:id', EmployeeController.delete)
 
 module.exports = router
